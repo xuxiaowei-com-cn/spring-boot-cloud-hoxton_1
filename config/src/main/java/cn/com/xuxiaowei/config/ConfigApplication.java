2612e72d1dd53492c5161a49cba9a826e5c1910e
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-01-15 17:17:12 +0800

    配置

---
+++ config/src/main/java/cn/com/xuxiaowei/config/ConfigApplication.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.com.xuxiaowei.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 配置 程序执行入口
+ * <p>
+ * Spring Cloud 使用注解 {@link EnableEurekaClient} 的方式提供 Eureka 客户端
+ * Spring Cloud 使用注解 {@link EnableConfigServer} 的方式提供 开启配置服务器
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@EnableEurekaClient
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
