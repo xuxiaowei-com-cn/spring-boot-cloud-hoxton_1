010d3a5ba13b6bd7dd3e37bfd52b9df911321496
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-01-15 15:26:40 +0800

    Initial commit

---
+++ src/main/java/cn/com/xuxiaowei/SpringBootCloudHoxtonApplication.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCloudHoxtonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCloudHoxtonApplication.class, args);
+    }
+
+}
