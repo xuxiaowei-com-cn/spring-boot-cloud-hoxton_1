cd719a60f2d524b2a82644aa298f40075517cb5c
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-01-16 16:55:38 +0800

    界面

---
+++ ui/src/main/java/cn/com/xuxiaowei/ui/UiApplication.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cn.com.xuxiaowei.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * UI(Ribbon、Feign)(界面模块) 程序执行入口
+ * <p>
+ * 通过 {@link EnableFeignClients} 开启 feign 客户端支持
+ * 通过 {@link EnableCircuitBreaker} 开启 CircuitBreaker 的支持
+ * 通过 {@link EnableZuulProxy} 开启网关代理的支持
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@EnableEurekaClient
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableZuulProxy
+@SpringBootApplication
+public class UiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UiApplication.class, args);
+    }
+
+}
