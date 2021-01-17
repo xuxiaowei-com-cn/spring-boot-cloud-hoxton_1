0bbbc065c64d47f609b700fc60922b1a9ea86ac9
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-01-17 16:04:11 +0800

    发送至服务时，携带 header

---
+++ ui/src/main/java/cn/com/xuxiaowei/ui/configuration/FeignClientsConfigurationCustom.java
@@ -0,0 +1,49 @@
+package cn.com.xuxiaowei.ui.configuration;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+/**
+ * 发送至服务时，携带 header
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@Configuration
+public class FeignClientsConfigurationCustom implements RequestInterceptor {
+
+    @Override
+    public void apply(RequestTemplate template) {
+
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        if (requestAttributes == null) {
+            return;
+        }
+
+        ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) requestAttributes;
+        HttpServletRequest request = servletRequestAttributes.getRequest();
+        Enumeration<String> headerNames = request.getHeaderNames();
+
+        if (headerNames != null) {
+            while (headerNames.hasMoreElements()) {
+                String headerName = headerNames.nextElement();
+                Enumeration<String> values = request.getHeaders(headerName);
+                while (values.hasMoreElements()) {
+                    String headerValue = values.nextElement();
+                    template.header(headerName, headerValue);
+                }
+            }
+        }
+
+    }
+
+}
