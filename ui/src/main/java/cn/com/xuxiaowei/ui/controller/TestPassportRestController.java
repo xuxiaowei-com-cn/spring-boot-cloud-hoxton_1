15aa8765a6176760f7c7ba3a11aa230c768ed9c9
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-01-17 12:25:15 +0800

    测试 redis session

--- ui/src/main/java/cn/com/xuxiaowei/ui/controller/TestPassportRestController.java
+++ ui/src/main/java/cn/com/xuxiaowei/ui/controller/TestPassportRestController.java
@@ -16,12 +16,14 @@
 package cn.com.xuxiaowei.ui.controller;
 
 import cn.com.xuxiaowei.ui.service.hystrix.TesstPassportHystrixService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
@@ -32,6 +34,7 @@ import java.util.Map;
  * @author xuxiaowei
  * @since 0.0.1
  */
+@Slf4j
 @RestController
 @RequestMapping("/test/passport")
 public class TestPassportRestController {
@@ -48,10 +51,14 @@ public class TestPassportRestController {
      *
      * @param request  请求
      * @param response 响应
+     * @param session  session
      * @return 返回 测试 登录模块 结果
      */
     @RequestMapping(value = "/testMsg")
-    public Map<String, Object> testMsg(HttpServletRequest request, HttpServletResponse response) {
+    public Map<String, Object> testMsg(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+
+        log.info("session id：{}", session.getId());
+
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
         String nowFormat = now.format(dateTimeFormatter);
@@ -63,11 +70,15 @@ public class TestPassportRestController {
      *
      * @param request  请求
      * @param response 响应
+     * @param session  session
      * @param testMsg  测试参数，必填
      * @return 返回 测试 登录模块 结果
      */
     @RequestMapping(value = "/testMsg", params = {"testMsg"})
-    public Map<String, Object> testMsg(HttpServletRequest request, HttpServletResponse response, String testMsg) {
+    public Map<String, Object> testMsg(HttpServletRequest request, HttpServletResponse response, HttpSession session, String testMsg) {
+
+        log.info("session id：{}", session.getId());
+
         return tesstPassportHystrixService.testMsg(testMsg);
     }
 
