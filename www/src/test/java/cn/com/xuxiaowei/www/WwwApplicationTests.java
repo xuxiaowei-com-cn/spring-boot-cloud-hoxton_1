d3c257a8ea4e67d28fe28be609fee49abb50520a
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-01-19 21:43:05 +0800

    新增模块

---
+++ www/src/test/java/cn/com/xuxiaowei/www/WwwApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
