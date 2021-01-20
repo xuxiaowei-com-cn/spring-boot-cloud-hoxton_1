20de13300b3f2b3ea1188be61b2a94257c2f0855
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-01-20 12:11:19 +0800

    测试 参数接收、保存数据

--- www/src/main/java/cn/com/xuxiaowei/www/test/controller/WwwRestController.java
+++ www/src/main/java/cn/com/xuxiaowei/www/test/controller/WwwRestController.java
@@ -1,8 +1,18 @@
 package cn.com.xuxiaowei.www.test.controller;
 
 
+import cn.com.xuxiaowei.www.test.entity.Www;
+import cn.com.xuxiaowei.www.test.service.IWwwService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <p>
@@ -15,6 +25,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test/www")
 public class WwwRestController {
+
+    private IWwwService wwwService;
+
+    @Autowired
+    public void setWwwService(IWwwService wwwService) {
+        this.wwwService = wwwService;
+    }
+
+    /**
+     * 测试 参数接收、保存数据
+     *
+     * @param request  请求
+     * @param response 响应
+     * @param session  session
+     * @param www      网站模块测试表
+     * @return 返回 测试结果
+     */
+    @RequestMapping("/save")
+    public Map<String, Object> save(HttpServletRequest request, HttpServletResponse response, HttpSession session,
+                                    @RequestBody Www www) {
+
+        Map<String, Object> map = new HashMap<>(4);
+        Map<String, Object> data = new HashMap<>(4);
+        map.put("data", data);
+
+        www.setCreateUsername("xxw");
+        www.setCreateIp(request.getRemoteHost());
+
+        wwwService.save(www);
+
+        map.put("code", "00000");
+        map.put("msg", "保存成功");
+        map.put("Www Session ID", session.getId());
+        data.put("www", www);
+
+        return map;
+    }
 
 }
 
