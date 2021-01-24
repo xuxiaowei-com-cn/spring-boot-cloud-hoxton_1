2b43e4041e8b6c783e1c0701344204c3c6802247
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-01-24 22:09:10 +0800

    @see

--- oauth2/src/main/java/cn/com/xuxiaowei/cloud/oauth/Oauth2Application.java
+++ oauth2/src/main/java/cn/com/xuxiaowei/cloud/oauth/Oauth2Application.java
@@ -19,11 +19,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
+import org.springframework.security.oauth2.provider.endpoint.TokenEndpoint;
+
+import java.security.Principal;
+import java.util.Map;
 
 /**
  * OAuth2 服务 程序执行入口
  *
  * @author xuxiaowei
+ * @see TokenEndpoint#getAccessToken(Principal, Map)
+ * @see TokenEndpoint#postAccessToken(Principal, Map)
+ * @see BaseClientDetails#BaseClientDetails(String, String, String, String, String, String) 若 authorizedGrantTypes 为空，
+ * 设置默认值："authorization_code", "refresh_token"
  * @since 0.0.1
  */
 @EnableResourceServer
