00af054ff507e7f0fdb1d049f629dc7f1f9c0fb1
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-01-24 23:05:30 +0800

    @EnableResourceServer

--- ui/src/main/java/cn/com/xuxiaowei/cloud/ui/UiApplication.java
+++ ui/src/main/java/cn/com/xuxiaowei/cloud/ui/UiApplication.java
@@ -21,10 +21,12 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 /**
  * UI(Ribbon、Feign)(界面模块) 程序执行入口
  * <p>
+ * 通过 {@link EnableResourceServer} 开启资源保护
  * 通过 {@link EnableEurekaClient} 开启 Eureka 客户端。告诉服务，它应该使用 Eureka 服务发现代理注册本身，并且服务调用是使用服务发现来“查找”远程服务的位置的。
  * 通过 {@link EnableFeignClients} 开启 feign 客户端支持
  * 通过 {@link EnableCircuitBreaker} 开启 CircuitBreaker 的支持。使服务能够使用 Hystrix 和 Ribbon 库。
@@ -35,6 +37,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @see <a href="https://docs.spring.io/spring-cloud-netflix/docs/2.2.6.RELEASE/reference/html/#netflix-zuul-reverse-proxy">netflix-zuul-reverse-proxy</a>
  * @since 0.0.1
  */
+@EnableResourceServer
 @EnableEurekaClient
 @EnableFeignClients
 @EnableCircuitBreaker
