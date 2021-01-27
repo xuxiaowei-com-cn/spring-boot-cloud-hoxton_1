1db59a3dc5358888fb16a2e133f033c8ff9512a0
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-01-27 21:12:06 +0800

    资源服务器 配置

---
+++ ui/src/main/java/cn/com/xuxiaowei/cloud/ui/configuration/ResourceServerConfigurerAdapterConfiguration.java
@@ -0,0 +1,16 @@
+package cn.com.xuxiaowei.cloud.ui.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+/**
+ * 资源服务器 配置
+ *
+ * @author xuxiaowei
+ * @since 0.0.1
+ */
+@Configuration
+public class ResourceServerConfigurerAdapterConfiguration extends ResourceServerConfigurerAdapter {
+
+
+}
