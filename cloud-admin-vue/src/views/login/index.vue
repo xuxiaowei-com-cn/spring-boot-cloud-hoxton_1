9d741343be74e04834d148259baa0dc83b0d042d
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-01-28 13:30:08 +0800

    登录 form

--- cloud-admin-vue/src/views/login/index.vue
+++ cloud-admin-vue/src/views/login/index.vue
@@ -1,15 +1,115 @@
 <template>
-  <div class="login">
-    <h1>Login</h1>
-  </div>
+  <el-container class="login">
+    <el-main class="login-main">
+      <el-form ref="form" :model="loginForm" class="login-form">
+        <el-form-item>
+          <el-input type="text" v-model="loginForm.username"
+                    :autocomplete="loginFormAutocomplete.username"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input type="password" v-model="loginForm.password"
+                    :autocomplete="loginFormAutocomplete.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox v-model="loginForm.rememberMe">记住我</el-checkbox>
+          <el-link type="primary" target="_blank" :underline="false" href="#forget" class="forget">忘记密码？</el-link>
+        </el-form-item>
+        <el-form-item>
+          <el-button @click="login()" class="login">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-main>
+  </el-container>
 </template>
 
 <script setup>
+import {reactive} from 'vue'
+
+// 登录参数
+const loginForm = reactive({
+  username: null,
+  password: null,
+  rememberMe: false,
+})
+
+// 是否自动填充
+const loginFormAutocomplete = reactive({
+  username: 'on',
+  password: 'off',
+})
+
+function login() {
+  console.log(loginForm)
+}
 
 </script>
 
 <style lang="scss">
+.login {
+  .login-main {
 
+    .login-form {
+      margin-left: auto;
+      margin-right: auto;
+
+      .el-form-item {
+        .forget {
+          float: right;
+        }
+
+        .login {
+          width: 100%;
+        }
+
+        .el-form-item__content {
+          margin-left: auto;
+          margin-right: auto;
+        }
+      }
+    }
+
+    /* 大于手机屏 */
+    @media screen and (max-width: 768px) {
+      .login-form {
+        width: 300px;
+
+        .el-form-item {
+          .el-form-item__content {
+            width: 280px;
+          }
+        }
+      }
+    }
+
+    /* 手机屏 */
+    @media screen and (min-width: 768px) {
+      .login-form {
+        width: 375px;
+
+        .el-form-item {
+          .el-form-item__content {
+            width: 335px;
+          }
+        }
+      }
+    }
+  }
+
+  /* 大于手机屏 */
+  @media screen and (max-width: 768px) {
+    .login-main {
+      padding: 110px 0;
+    }
+  }
+
+  /* 手机屏 */
+  @media screen and (min-width: 768px) {
+    .login-main {
+      padding: 60px 0 110px 0;
+    }
+  }
+
+}
 </style>
 
 <style scoped>
