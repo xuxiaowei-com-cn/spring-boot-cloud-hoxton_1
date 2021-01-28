699ac419ac392d2ca131fc7e4b1b0760bf78a9ec
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-01-28 21:39:21 +0800

    request

--- cloud-admin-vue/src/main.js
+++ cloud-admin-vue/src/main.js
@@ -7,10 +7,13 @@ import './styles/main.scss'
 import ElementPlus from 'element-plus'
 
 import router from './router'
+import request from './utils/request'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(ElementPlus)
+
+app.config.globalProperties.request = request
 
 app.mount('#app')
