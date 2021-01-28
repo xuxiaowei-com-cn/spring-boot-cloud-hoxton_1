b41a4e265ae13b5f4e5023c0e0b8ec50a94701ba
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-01-28 12:25:14 +0800

    oauth2-pom

---
+++ oauth2-pom/src/main/java/cn/com/xuxiaowei/cloud/pom/Oauth2PomApplication.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.cloud.pom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2PomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2PomApplication.class, args);
+    }
+
+}
---
+++ oauth2-pom/src/test/java/cn/com/xuxiaowei/cloud/pom/Oauth2PomApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.cloud.pom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2PomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
