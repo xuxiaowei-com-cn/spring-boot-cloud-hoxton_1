b7ee0c03ccf20130aa5523f1d98511528f91c8ce
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-01-30 23:07:58 +0800

    package

--- cloud-security-utils/src/main/java/cn/com/xuxiaowei/cloud/utils/security/CloudSecurityUtilsApplication.java
+++ cloud-security-utils/src/main/java/cn/com/xuxiaowei/cloud/utils/security/CloudSecurityUtilsApplication.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package cn.com.xuxiaowei.cloud.utils;
+package cn.com.xuxiaowei.cloud.utils.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- cloud-security-utils/src/test/java/cn/com/xuxiaowei/cloud/utils/security/CloudSecurityUtilsApplicationTests.java
+++ cloud-security-utils/src/test/java/cn/com/xuxiaowei/cloud/utils/security/CloudSecurityUtilsApplicationTests.java
@@ -1,4 +1,4 @@
-package cn.com.xuxiaowei.cloud.utils;
+package cn.com.xuxiaowei.cloud.utils.security;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
