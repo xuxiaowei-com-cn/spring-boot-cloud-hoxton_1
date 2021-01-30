1b228a9845d9aa47fa90fcdeaaa83caea762e3b1
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-01-30 22:53:34 +0800

    cloud-security-utils

---
+++ cloud-security-utils/src/main/java/cn/com/xuxiaowei/cloud/utils/CloudSecurityUtilsApplication.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.cloud.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSecurityUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudSecurityUtilsApplication.class, args);
+    }
+
+}
