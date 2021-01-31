53c82a20f23a8f09250de4cfcd300c465884db8d
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2021-01-31 14:21:23 +0800

    登录配置

--- passport/src/main/java/cn/com/xuxiaowei/cloud/passport/configuration/WebSecurityConfigurerAdapterConfiguration.java
+++ passport/src/main/java/cn/com/xuxiaowei/cloud/passport/configuration/WebSecurityConfigurerAdapterConfiguration.java
@@ -15,11 +15,25 @@
  */
 package cn.com.xuxiaowei.cloud.passport.configuration;
 
+import cn.com.xuxiaowei.cloud.passport.properties.HttpSecurityFormLoginDefaultProperties;
+import cn.com.xuxiaowei.cloud.utils.security.filter.CsrfCookieBeforeOncePerRequestFilter;
+import cn.com.xuxiaowei.cloud.utils.security.properties.CsrfCookieDefaultProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import javax.sql.DataSource;
 
 /**
  * WebSecurity 配置
@@ -30,20 +44,103 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class WebSecurityConfigurerAdapterConfiguration extends WebSecurityConfigurerAdapter {
 
+    private DataSource dataSource;
+
+    private CsrfTokenRepository csrfTokenRepository;
+
+    private CsrfCookieDefaultProperties csrfCookieDefaultProperties;
+
     private UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource;
+
+    private HttpSecurityFormLoginDefaultProperties httpSecurityFormLoginDefaultProperties;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Autowired
+    public void setCsrfTokenRepository(CsrfTokenRepository csrfTokenRepository) {
+        this.csrfTokenRepository = csrfTokenRepository;
+    }
+
+    @Autowired
+    public void setCsrfCookieDefaultProperties(CsrfCookieDefaultProperties csrfCookieDefaultProperties) {
+        this.csrfCookieDefaultProperties = csrfCookieDefaultProperties;
+    }
 
     @Autowired
     public void setUrlBasedCorsConfigurationSource(UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource) {
         this.urlBasedCorsConfigurationSource = urlBasedCorsConfigurationSource;
     }
 
+    @Autowired
+    public void setHttpSecurityFormLoginDefaultProperties(HttpSecurityFormLoginDefaultProperties httpSecurityFormLoginDefaultProperties) {
+        this.httpSecurityFormLoginDefaultProperties = httpSecurityFormLoginDefaultProperties;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        return super.userDetailsService();
+    }
+
+    /**
+     * @see super#configure(HttpSecurity)
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+
+        // Form Login 配置
+        http.formLogin()
+                // 登录页面
+                .loginPage(httpSecurityFormLoginDefaultProperties.getLoginPage())
+                // 登录请求
+                .loginProcessingUrl(httpSecurityFormLoginDefaultProperties.getLoginProcessingUrl())
+                // 登录成功
+                .successForwardUrl(httpSecurityFormLoginDefaultProperties.getSuccessForwardUrl())
+                // 登录失败
+                .failureForwardUrl(httpSecurityFormLoginDefaultProperties.getFailureForwardUrl())
+                .permitAll()
+        ;
+
+        // 权限配置
+        http.authorizeRequests().anyRequest().authenticated();
+
+        // CSRF 配置
+        http.csrf().csrfTokenRepository(csrfTokenRepository);
+        http.addFilterBefore(new CsrfCookieBeforeOncePerRequestFilter(csrfTokenRepository, csrfCookieDefaultProperties),
+                CsrfFilter.class);
 
         // Security CORS 配置
         http.cors().configurationSource(urlBasedCorsConfigurationSource);
 
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // 用户密码编辑器
+        PasswordEncoder delegatingPasswordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        // 查询登录用户
+        auth.userDetailsService(jdbcDaoImpl()).passwordEncoder(delegatingPasswordEncoder);
+
+    }
+
+    /**
+     * 登录时查询用户
+     */
+    public JdbcDaoImpl jdbcDaoImpl() {
+        JdbcDaoImpl jdbcDao = new JdbcDaoImpl();
+        jdbcDao.setDataSource(dataSource);
+        return jdbcDao;
     }
 
 }
